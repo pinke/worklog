a5952c700dbeb49fc0369b70838a77266f317640
pinke <xpinke@gmail.com>
2016-02-21 00:03:47 +0800

    limit show current user logs.

--- worklog/files/worklog.js
+++ worklog/files/worklog.js
@@ -1,3 +1,4 @@
 $(document).ready(function () {
-    $("input[name=log_begin],input[name=log_end]").datepicker({dateFormat:'yy-mm-dd'});
+    $("input[name=log_begin]:not(:read-only),input[name=log_end]:not(:read-only)")
+        .datepicker({dateFormat:'yy-mm-dd',changeMonth: true,changeYear: true});
 });
