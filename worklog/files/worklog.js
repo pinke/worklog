ef9fe9630c28b9ed4ba43c7fb45094189fb613c7
pinke <xpinke@gmail.com>
2016-02-20 23:27:41 +0800

    fixed somthing..

--- worklog/files/worklog.js
+++ worklog/files/worklog.js
@@ -1,3 +1,3 @@
 $(document).ready(function () {
-    $("input[name=log_begin],input[name=log_end]").datepicker({format:'yy-mm-dd',altFormat:'yy-mm-dd'});
+    $("input[name=log_begin],input[name=log_end]").datepicker({dateFormat:'yy-mm-dd'});
 });
