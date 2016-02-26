47fdb620ac21679fe95aca504ca70aac86238d29
pinke <xpinke@gmail.com>
2016-02-26 17:00:01 +0800

    添加周报模板

--- worklog/files/worklog.js
+++ worklog/files/worklog.js
@@ -1,22 +1,28 @@
-function page_add_onLogTypeChange(logType){
-    var contentEl=$('textarea[name="content"]');
-    if (logType = 1) {
-        if (!$.trim(contentEl.val()) || confirm('是否清空内容?'))
-            contentEl.val('##原计划工作内容\n' +
-                ' - \n' +
-                '##实际工作内容\n' +
-                ' - \n' +
-                '##工作成果说明\n' +
-                ' - \n' +
-                '##完成率\n' +
-                ' - \n' +
-                '##下周计划和资源需求'
-            );
+//TODO:config it
+var weeklog_content_template = {
+    'type-1': '##原计划工作内容\n' +
+    ' - \n' +
+    '##实际工作内容\n' +
+    ' - \n' +
+    '##工作成果说明\n' +
+    ' - \n' +
+    '##完成率\n' +
+    ' - \n' +
+    '##下周计划和资源需求'
+};
+
+function weeklog_page_add_onLogTypeChange(logType) {
+    var contentEl = $('textarea[name="content"]');
+    var tpl;
+    if ((tpl = weeklog_content_template['type-' + logType]) && contentEl.val() != tpl) {
+        if (!$.trim(contentEl.val()) || confirm('是否清空内容?')) {
+            contentEl.val(weeklog_content_template['type-' + logType]);
+        }
     }
 }
 $(document).ready(function () {
     $("input[name=log_begin]:not(:read-only),input[name=log_end]:not(:read-only)")
-        .datepicker({dateFormat:'yy-mm-dd',changeMonth: true,changeYear: true});
+        .datepicker({dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true});
 
 
 });
