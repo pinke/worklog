a3c746b1f1c03c2ebaa5929ed38e3f0ff8279989
pinke <xpinke@gmail.com>
2017-02-09 15:18:22 +0800

    fixed:delete page

--- worklog/pages/worklog_delete_page.php
+++ worklog/pages/worklog_delete_page.php
@@ -9,7 +9,9 @@ $f_id = gpc_get_int( 'f_id' );
 ?>
 <p>
 <div align="center">
-	<?php print_hr( $g_hr_size, $g_hr_width ) ?>
+	<?php
+//    print_hr( $g_hr_size, $g_hr_width )
+    ?>
 	<?php echo plugin_lang_get( 'delete_worklog_sure_msg' ) . "<br>" ?>
 
 	<form method="post" action="<?php echo $g_worklog_delete ?>">
@@ -17,7 +19,9 @@ $f_id = gpc_get_int( 'f_id' );
 		<input type="submit" value="<?php echo plugin_lang_get( 'delete_worklog_item_button' ) ?>">
 	</form>
 
-	<?php print_hr( $g_hr_size, $g_hr_width ) ?>
+	<?php
+//    print_hr( $g_hr_size, $g_hr_width )
+    ?>
 </div>
 
 <?php
